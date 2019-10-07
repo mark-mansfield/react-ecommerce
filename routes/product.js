927e7a77d0e878ae5e9c8f45827371badedf010a
mark-mansfield <markgmansfield@gmail.com>
2019-10-08 10:59:13 +1100

    product-update merged

--- routes/product.js
+++ routes/product.js
@@ -1,30 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read , remove, update} = require('../controllers/product');
+const { create, productById, read, remove, update } = require('../controllers/product');
 const { requireSignIn, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
-router.put('/product/:productId/:userId',
-    requireSignIn,
-    isAdmin,
-    isAuth,
-    update);
-
+router.put('/product/:productId/:userId', requireSignIn, isAdmin, isAuth, update);
 
 router.get('/product/:productId', read);
 
-router.delete('/product/:productId/:userId',
-    requireSignIn,
-    isAdmin,
-    isAuth,
-    remove);
+router.delete('/product/:productId/:userId', requireSignIn, isAdmin, isAuth, remove);
 
-router.post('/product/create/:userId',
-    requireSignIn,
-    isAdmin,
-    isAuth,
-    create);
+router.post('/product/create/:userId', requireSignIn, isAdmin, isAuth, create);
 
 router.param('userId', userById);
 router.param('productId', productById);
