91fd003fb8d736918ed1e1d8234b684f0d200b6f
mark-mansfield <markgmansfield@gmail.com>
2019-10-10 10:22:49 +1100

    user admin routes added

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,23 @@ exports.userById = (req, res, next, id) => {
     next();
   });
 };
+
+exports.read = (req, res) => {
+  // do not return the password or salt
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
+
+exports.update = (req, res) => {
+  user.findOneAndUpdate({ _id: req.profile._id }, { $set: req.body }, { new: true }, (err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'You are not allowed to perform this action'
+      });
+    }
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    res.json(user);
+  });
+};
