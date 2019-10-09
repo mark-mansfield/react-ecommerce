5d749b38695ccc2dec485b06c781d3b59ccfa34d
mark-mansfield <markgmansfield@gmail.com>
2019-10-10 10:04:29 +1100

    get product photos working

--- routes/product.js
+++ routes/product.js
@@ -10,7 +10,8 @@ const {
   list,
   listRelated,
   listCategories,
-  listBySearch
+  listBySearch,
+  photo
 } = require('../controllers/product');
 
 const { requireSignIn, isAuth, isAdmin } = require('../controllers/auth');
@@ -24,7 +25,7 @@ router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.post('/products/by/search', listBySearch);
 router.get('/products/categories/', listCategories);
-
+router.get('/products/photo/:productId', photo);
 router.param('userId', userById);
 router.param('productId', productById);
 module.exports = router;
